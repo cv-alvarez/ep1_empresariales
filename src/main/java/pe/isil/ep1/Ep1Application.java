793a6703a8260fda94fc96860d46c3c5a5fdd7b8
Christian Valdivia <cva.250199@gmail.com>
2022-09-25 23:13:24 -0500

    ep1

---
+++ src/main/java/pe/isil/ep1/Ep1Application.java
@@ -0,0 +1,15 @@
+package pe.isil.ep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep1Application.class, args);
+		System.out.print("hola mundo");
+	}
+
+
+}
---
+++ src/test/java/pe/isil/ep1/Ep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.ep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
